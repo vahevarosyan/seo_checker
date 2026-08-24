18c0c4bf8f7b2f8b49da38c3f2685710b2e77ce7
vahevarosyan <vahe.varossyan@gmail.com>
2026-08-24 16:01:18 +0400

    Update fetch logic to use Scraper API
    Refactor fetch logic to use Scraper API and improve response structure.

--- api/fetch-meta.js
+++ api/fetch-meta.js
@@ -13,18 +13,18 @@ export default async function handler(req) {
 
   if (!/^https?:\/\//i.test(url)) url = 'https://' + url;
 
+  const apiKey = process.env.SCRAPER_API_KEY;
+
   try {
-    const res = await fetch(url, {
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (compatible; MetaCheckBot/1.0)',
-        'Accept': 'text/html'
-      },
-      redirect: 'follow',
-      signal: AbortSignal.timeout(8000)
+    const scraperUrl = 'http://api.scraperapi.com?api_key=' + apiKey + '&url=' + encodeURIComponent(url) + '&render=false';
+
+    const res = await fetch(scraperUrl, {
+      headers: { 'Accept': 'text/html' },
+      signal: AbortSignal.timeout(15000)
     });
 
     if (!res.ok) {
-      return new Response(JSON.stringify({ error: `Failed to fetch: ${res.status}` }), {
+      return new Response(JSON.stringify({ error: 'Failed to fetch: ' + res.status }), {
         status: 200,
         headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }
       });
@@ -46,17 +46,20 @@ export default async function handler(req) {
                  || get(/<meta[^>]+content=["']([^"']*)["'][^>]+property=["']og:description["']/i);
     const ogUrl   = get(/<meta[^>]+property=["']og:url["'][^>]+content=["']([^"']*)["']/i)
                  || get(/<meta[^>]+content=["']([^"']*)["'][^>]+property=["']og:url["']/i);
+    const ogImage = get(/<meta[^>]+property=["']og:image["'][^>]+content=["']([^"']*)["']/i)
+                 || get(/<meta[^>]+content=["']([^"']*)["'][^>]+property=["']og:image["']/i);
     const twTitle = get(/<meta[^>]+name=["']twitter:title["'][^>]+content=["']([^"']*)["']/i)
                  || get(/<meta[^>]+content=["']([^"']*)["'][^>]+name=["']twitter:title["']/i);
     const twDesc  = get(/<meta[^>]+name=["']twitter:description["'][^>]+content=["']([^"']*)["']/i)
                  || get(/<meta[^>]+content=["']([^"']*)["'][^>]+name=["']twitter:description["']/i);
-    const ogImage = get(/<meta[^>]+property=["']og:image["'][^>]+content=["']([^"']*)["']/i)
-                 || get(/<meta[^>]+content=["']([^"']*)["'][^>]+property=["']og:image["']/i);
     const twImage = get(/<meta[^>]+name=["']twitter:image["'][^>]+content=["']([^"']*)["']/i)
                  || get(/<meta[^>]+content=["']([^"']*)["'][^>]+name=["']twitter:image["']/i)
                  || get(/<meta[^>]+name=["']twitter:image:src["'][^>]+content=["']([^"']*)["']/i);
 
-    return new Response(JSON.stringify({ title, desc, ogTitle, ogDesc, ogUrl, ogImage, twTitle, twDesc, twImage, fetchedUrl: res.url }), {
+    return new Response(JSON.stringify({
+      title, desc, ogTitle, ogDesc, ogUrl, ogImage,
+      twTitle, twDesc, twImage, fetchedUrl: url
+    }), {
       status: 200,
       headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }
     });
